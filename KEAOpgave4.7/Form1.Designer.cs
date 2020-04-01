4bf493f981bcc75b47fba7f7d0da2c32179737bf
skrrrm <a@macbook-windows>
2020-04-01 17:45:44 +0200

    Add project files.

---
+++ KEAOpgave4.7/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace KEAOpgave4._7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.buttonCalc = new System.Windows.Forms.Button();
+            this.textBoxLoops = new System.Windows.Forms.TextBox();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.ItemHeight = 25;
+            this.listBoxResult.Location = new System.Drawing.Point(72, 88);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(221, 754);
+            this.listBoxResult.TabIndex = 0;
+            // 
+            // buttonCalc
+            // 
+            this.buttonCalc.Location = new System.Drawing.Point(341, 358);
+            this.buttonCalc.Name = "buttonCalc";
+            this.buttonCalc.Size = new System.Drawing.Size(194, 49);
+            this.buttonCalc.TabIndex = 1;
+            this.buttonCalc.Text = "Calculate";
+            this.buttonCalc.UseVisualStyleBackColor = true;
+            this.buttonCalc.Click += new System.EventHandler(this.buttonCalc_Click);
+            // 
+            // textBoxLoops
+            // 
+            this.textBoxLoops.Location = new System.Drawing.Point(341, 195);
+            this.textBoxLoops.Name = "textBoxLoops";
+            this.textBoxLoops.Size = new System.Drawing.Size(194, 31);
+            this.textBoxLoops.TabIndex = 2;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(341, 143);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(175, 25);
+            this.labelInfo.TabIndex = 3;
+            this.labelInfo.Text = "Number of loops:";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(72, 57);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(84, 25);
+            this.labelResult.TabIndex = 4;
+            this.labelResult.Text = "Values:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 867);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.textBoxLoops);
+            this.Controls.Add(this.buttonCalc);
+            this.Controls.Add(this.listBoxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.Button buttonCalc;
+        private System.Windows.Forms.TextBox textBoxLoops;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ KEAOpgave4.7/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KEAOpgave4._7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void buttonCalc_Click(object sender, EventArgs e)
+        {
+            listBoxResult.Items.Clear();
+
+            // Variables
+            double harmonic = 1.0;
+            int counter = 0;
+
+            for (int n = 2; harmonic <= 5; n++)
+            {
+                harmonic += (double)1 / n;
+                listBoxResult.Items.Add($"{harmonic:F5}");
+                counter++;
+                textBoxLoops.Text = $"{counter}";
+            }
+        }
+    }
+}
